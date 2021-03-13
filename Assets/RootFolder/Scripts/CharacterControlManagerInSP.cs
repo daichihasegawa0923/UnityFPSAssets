67efe1430fcc2ea32afa4557f7949c9fe4207544
daichihasegawa0923 <daichihasegawa052@gmail.com>
2021-03-13 13:19:43 +0900

    Add changing perspective in smartphone.

--- Assets/RootFolder/Scripts/CharacterControlManagerInSP.cs
+++ Assets/RootFolder/Scripts/CharacterControlManagerInSP.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace Diamond.CharacterControlLib
@@ -22,17 +23,32 @@ namespace Diamond.CharacterControlLib
         [SerializeField]
         protected Joystick _joystick;
 
+        /// <summary>
+        /// 視点変更のためのDrag用のイベントトリガー
+        /// </summary>
+        [SerializeField]
+        protected EventTrigger _eventTriggerDragChangePerspective;
+
         [SerializeField]
         protected Button _buttonJump;
 
         protected virtual void Start()
         {
             _buttonJump.onClick.AddListener(_characterController.Jump);
+            AttachDragEvent();
         }
 
         protected virtual void Update()
         {
             MoveByJoystick();
+        }
+
+        protected virtual void AttachDragEvent()
+        {
+            var entry = new EventTrigger.Entry();
+            entry.eventID = EventTriggerType.Drag;
+            entry.callback.AddListener(d => DragForChangePerspective((PointerEventData)d));
+            _eventTriggerDragChangePerspective.triggers.Add(entry);
         }
 
         /// <summary>
@@ -44,6 +60,19 @@ namespace Diamond.CharacterControlLib
             direction.x = _joystick.Horizontal;
             direction.z = _joystick.Vertical;
             _characterController.Move(direction);
+        }
+
+        /// <summary>
+        /// ドラッグした方向に合わせて視点を移動する
+        /// </summary>
+        /// <param name="pointerEventData"></param>
+        public void DragForChangePerspective(PointerEventData pointerEventData)
+        {
+            var spin = Vector3.zero;
+            spin.y = -pointerEventData.delta.x;
+            spin.x = pointerEventData.delta.y;
+
+            _characterController.ChangePerspective(spin);
         }
     }
 }
